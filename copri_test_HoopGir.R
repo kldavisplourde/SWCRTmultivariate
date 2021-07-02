083c961324dae1f09e293f5d874ecf1010411f08
Kendra Davis-Plourde <kendra.davis727@gmail.com>
2021-07-02 11:51:37 -0400

    No change
    Testing different parameters

--- copri_test_HoopGir.R
+++ copri_test_HoopGir.R
@@ -5,7 +5,7 @@ library(foreach)
 set.seed(5792) #10000 iterations
 
 simData <- foreach(i=1:2, .combine=rbind) %do% {
-  dt<-datagen_cont(n=15, m=20, K=2, cv=0, sigmac=matrix(c(1,0.12,0.12,1),2), sigmacp=matrix(c(1,0.18,0.18,1),2), sigmae= matrix(c(2.3,0.05,0.05,2.5),2), eff=c(1.2,0.5), time.eff=c(1.1,1.3,1.4,0.9,0.7,0.5))$short
+  dt<-datagen_cont(n=15, m=20, K=2, cv=0, sigmac=matrix(c(1,0.12,0.12,1),2), sigmacp=matrix(c(1,0.18,0.18,1),2), sigmae= matrix(c(2.3,0.1,0.1,2.5),2), eff=c(1.2,0.5), time.eff=c(1.1,1.3,1.4,0.9,0.7,0.5))$short
 
   lme1<-lmer(out1~time.1+time.2+time.3+arm +(1|cluster) +(1|cluster.period),data=dt)
   lme2<-lmer(out2~time.1+time.2+time.3+arm +(1|cluster) +(1|cluster.period),data=dt)
