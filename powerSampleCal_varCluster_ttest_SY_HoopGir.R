ec798e523b9637270ca2326d0f582c82d534f633
Kendra Davis-Plourde <kendra.davis727@gmail.com>
2022-04-14 21:49:37 -0400

    Update powerSampleCal_varCluster_ttest_SY_HoopGir.R
    Added additional scenarios

--- powerSampleCal_varCluster_ttest_SY_HoopGir.R
+++ powerSampleCal_varCluster_ttest_SY_HoopGir.R
@@ -267,12 +267,12 @@ calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2
 ### CAC=0.5
 rho01<-matrix(c(0.003,0,0,0.015),2)
 calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
-### rho_0^{12} = -0.02
+### rho_0^{12} = -0.002
 rho01<-matrix(c(0.00122,0,0,0.0059),2)
-rho02<-matrix(c(0.006,-0.02,-0.02,0.029),2)
+rho02<-matrix(c(0.006,-0.002,-0.002,0.029),2)
 calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
-### rho_0^{12} = 0.02
-rho02<-matrix(c(0.006,0.02,0.02,0.029),2)
+### rho_0^{12} = 0.002
+rho02<-matrix(c(0.006,0.002,0.002,0.029),2)
 calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
 ### rho_0^{2} = 0.023
 rho02<-matrix(c(0.006,0,0,0.023),2)
@@ -294,19 +294,19 @@ calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2
 rho2<-matrix(c(1,0.7,0.7,1),2)
 calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
 
-#Additional scenarios
+#Additional scenarios (up to 40%)
 ### CAC=0
 rho01<-matrix(c(0,0,0,0),2)
 calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
 ### CAC=0.8
 rho01<-matrix(c(0.0049,0,0,0.0235),2)
 calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
-### rho_0^{12} = -0.04
+### rho_0^{12} = -0.004
 rho01<-matrix(c(0.00122,0,0,0.0059),2)
-rho02<-matrix(c(0.006,-0.04,-0.04,0.029),2)
+rho02<-matrix(c(0.006,-0.004,-0.004,0.029),2)
 calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
-### rho_0^{12} = 0.029
-rho02<-matrix(c(0.006,0.029,0.029,0.029),2)
+### rho_0^{12} = 0.004
+rho02<-matrix(c(0.006,0.004,0.004,0.029),2)
 calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
 ### rho_0^{2} = 0.017
 rho02<-matrix(c(0.006,0,0,0.017),2)
@@ -326,4 +326,25 @@ rho2<-matrix(c(1,0.35,0.35,1),2)
 calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
 ### rho_2^{12} = 0.81
 rho2<-matrix(c(1,0.81,0.81,1),2)
+calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
+
+#Additional scenarios x2 (up to 60%)
+### rho_0^{2} = 0.012
+rho02<-matrix(c(0.006,0,0,0.012),2)
+calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
+### rho_0^{2} = 0.046
+rho02<-matrix(c(0.006,0,0,0.046),2)
+calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
+### rho_0^{1} = 0.002
+rho02<-matrix(c(0.002,0,0,0.029),2)
+calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
+### rho_0^{1} = 0.010
+rho02<-matrix(c(0.010,0,0,0.029),2)
+calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
+### rho_2^{12} = 0.23
+rho02<-matrix(c(0.006,0,0,0.029),2)
+rho2<-matrix(c(1,0.23,0.23,1),2)
+calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
+### rho_2^{12} = 0.93
+rho2<-matrix(c(1,0.93,0.93,1),2)
 calPower_IU(deltas,margins=c(0,0),vars=c(sd1^2,sd2^2),rho01,rho02,rho2,N,t,m,K=2,alpha=0.05)
